abf87bc7d6506fc2549e23810911eb1d0e5a4b18
Allan Cordeiro <allan.cordeiro.santos@gmail.com>
2023-03-02 21:12:34 -0300

    feat: add matches and match details pages

--- front-end/next/src/pages/matches/index.tsx
+++ front-end/next/src/pages/matches/index.tsx
@@ -1,13 +1,19 @@
-import { Button } from '@mui/material'
+import { Box, Button } from '@mui/material'
 import { NextPage } from 'next'
 
 import styles from '../styles/Home.module.css'
+import { Page } from '../../components/Page'
+import { MatchResult } from '../../components/MatchResult'
 
 const ListMatchesPage: NextPage = () => {
   return (
-    <div>
-      <Button variant='contained'>Teste</Button>
-    </div>
+    <Page>
+      <Box sx={{display: 'flex', alignItems:'center', flexDirection: 'column', gap: (theme) => theme.spacing(3)}}>
+        <MatchResult match={{team_a: 'Brasil', team_b: 'Argentina'}}/>
+        <MatchResult match={{team_a: 'Brasil', team_b: 'Argentina'}}/>
+        <MatchResult match={{team_a: 'Brasil', team_b: 'Argentina'}}/>
+      </Box>
+    </Page>
   )
 }
 
